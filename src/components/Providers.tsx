fd343e406ab52d1126256d4273d087585879cd0c
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-06-25 23:09:47 +0300

    feat(Providers.tsx): integrate React Query with DevTools for state management

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,17 @@
 "use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const client = new QueryClient();
 
 const Providers = ({ children }: PropsWithChildren<{}>) => {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default Providers;
