06e752aa0b853add06175df016637e26896c6985
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-21 21:16:28 +0300

    chore: Update imports

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,7 @@
+import { TProduct } from "@/types";
 import { Index } from "@upstash/vector";
 import * as dotenv from "dotenv";
 
 dotenv.config();
-export type TProduct = {
-  id: string;
-  imageId: string;
-  name: string;
-  size: "S" | "M" | "L";
-  color: "white" | "beige" | "blue" | "green" | "purple";
-  price: number;
-};
 
 export const db = new Index<TProduct>();
