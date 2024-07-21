9119ae056b5102d9b681fac53845cd039904a2a4
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-21 21:09:29 +0300

    feat: Add TProduct type definition

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type TProduct = {
+  id: string;
+  imageId: string;
+  name: string;
+  size: "S" | "M" | "L";
+  color: "white" | "beige" | "blue" | "green" | "purple";
+  price: number;
+};
