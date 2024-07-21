53fe839583f4ca509a01be27ca38c5f71a0fa716
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-21 22:25:08 +0300

    style: Update EmptyState component styling

--- src/components/products/EmptyState.tsx
+++ src/components/products/EmptyState.tsx
@@ -3,10 +3,13 @@ import { XCircle } from "lucide-react";
 const EmptyState = () => {
   return (
     <div className="relative col-span-full h-80 bg-gray-50 w-full p-12 flex flex-col items-center justify-center">
-      <XCircle className="h-8 w-8 text-red-500" />
-      <h3 className="font-semibold text-xl">No products found</h3>
-      <p className="text-zinc-500 text-sm">
-        We found no search results for your query. Please try again.
+      <XCircle className="h-10 w-10 text-red-500" />
+      <h3 className="font-bold my-2 text-stone-900  text-3xl capitalize ">
+        No products found
+      </h3>
+      <p className="text-zinc-500 text-sm  capitalize ">
+        No search results match your query. Please adjust your search and try
+        again.
       </p>
     </div>
   );
