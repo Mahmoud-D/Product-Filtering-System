e7c23a3629208f7525b8df06c5e281447e890313
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-27 23:52:41 +0300

    style: apply Prettier formatting to product-validator.ts

--- src/lib/validators/product-validator.ts
+++ src/lib/validators/product-validator.ts
@@ -1,14 +1,14 @@
-import { AVAILABLE_COLORS, AVAILABLE_SIZES, AVAILABLE_SORT } from "@/constants";
-import { z } from "zod";
+import { AVAILABLE_COLORS, AVAILABLE_SIZES, AVAILABLE_SORT } from '@/constants'
+import { z } from 'zod'
 
 export const productFilterValidator = z.object({
   size: z.array(z.enum(AVAILABLE_SIZES)),
   color: z.array(z.enum(AVAILABLE_COLORS)),
   sort: z.enum(AVAILABLE_SORT),
-  price: z.tuple([z.number(), z.number()]),
-});
+  price: z.tuple([z.number(), z.number()])
+})
 
 export type TProductState = Omit<
   z.infer<typeof productFilterValidator>,
-  "price"
-> & { price: { isCustom: boolean; range: [number, number] } };
+  'price'
+> & { price: { isCustom: boolean; range: [number, number] } }
