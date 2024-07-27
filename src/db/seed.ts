28596f6a0043e5748fcad0c7a1379454e716ad61
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-27 23:53:17 +0300

    style: apply Prettier formatting to seed.ts

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,26 +1,26 @@
-import { db } from ".";
-import { type TProduct } from "@/types";
-import * as dotenv from "dotenv";
+import { db } from '.'
+import { type TProduct } from '@/types'
+import * as dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 const getRandomPrice = () => {
-  const PRICES = [9.99, 19.99, 29.99, 39.99, 49.99];
-  return PRICES[Math.floor(Math.random() * PRICES.length)];
-};
+  const PRICES = [9.99, 19.99, 29.99, 39.99, 49.99]
+  return PRICES[Math.floor(Math.random() * PRICES.length)]
+}
 
-const COLORS = ["white", "beige", "blue", "green", "purple"] as const;
-const SIZES = ["S", "M", "L"] as const;
+const COLORS = ['white', 'beige', 'blue', 'green', 'purple'] as const
+const SIZES = ['S', 'M', 'L'] as const
 
 const seed = async () => {
-  const products: TProduct[] = [];
+  const products: TProduct[] = []
 
   // 3 example products
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < COLORS.length; j++) {
       for (let k = 0; k < SIZES.length; k++) {
-        const size = SIZES[k];
-        const color = COLORS[j];
+        const size = SIZES[k]
+        const color = COLORS[j]
         products.push({
           id: `${color}-${size}-${i + 1}`,
           imageId: `/${color}_${i + 1}.png`,
@@ -29,8 +29,8 @@ const seed = async () => {
             color.slice(0, 1).toUpperCase() + color.slice(1)
           } shirt ${i}`,
           size,
-          price: getRandomPrice(),
-        });
+          price: getRandomPrice()
+        })
       }
     }
   }
@@ -38,24 +38,24 @@ const seed = async () => {
   const SIZE_MAP = {
     S: 0,
     M: 1,
-    L: 2,
-  };
+    L: 2
+  }
 
   const COLOR_MAP = {
     white: 0,
     beige: 1,
     blue: 2,
     green: 3,
-    purple: 4,
-  };
+    purple: 4
+  }
 
   await db.upsert(
     products.map((product) => ({
       id: product.id,
       vector: [COLOR_MAP[product.color], SIZE_MAP[product.size], product.price],
-      metadata: product,
+      metadata: product
     }))
-  );
-};
+  )
+}
 
-seed();
+seed()
