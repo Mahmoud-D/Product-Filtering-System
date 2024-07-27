b8b86bca5332b1494ba2f8866474ef953f73d45f
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-28 00:23:42 +0300

    style: apply Prettier formatting

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
-import { TProduct } from "@/types";
-import { Index } from "@upstash/vector";
-import * as dotenv from "dotenv";
+import { TProduct } from '@/types'
+import { Index } from '@upstash/vector'
+import * as dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-export const db = new Index<TProduct>();
+export const db = new Index<TProduct>()
