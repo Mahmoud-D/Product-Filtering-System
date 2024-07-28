e431b9ba6f198d576a5fbe90c89f0407457b3bb0
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-28 20:17:16 +0300

    feat: Add language switcher component

---
+++ src/components/LangSwitcher.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useLocale, useTranslations } from 'next-intl'
+
+import { locales } from '@/config/locale'
+import { usePathname, useRouter } from '@/lib/navigation'
+
+type TProps = {}
+const LangSwitcher = ({}: TProps) => {
+  const t = useTranslations('SwitchLang')
+  const router = useRouter()
+  const pathname = usePathname()
+  const local = useLocale()
+
+  function handleLocaleChange(locale: 'en' | 'ar') {
+    router.push(pathname, { locale })
+  }
+
+  return (
+    <div className="flex gap-2">
+      {locales?.map((item) => (
+        <button
+          key={item}
+          onClick={() => handleLocaleChange(item as 'en' | 'ar')}
+          className={`h-10 rounded-md px-4 py-2 text-sm font-bold ${
+            local === item
+              ? 'bg-sky-900 text-white hover:bg-sky-800'
+              : 'bg-gray-200 text-black hover:bg-gray-300 dark:bg-slate-700 dark:text-white'
+          }`}
+        >
+          {t('locale', { locale: item })}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default LangSwitcher
