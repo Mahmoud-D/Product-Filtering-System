04bda1f51ec1e007bd027b86ecc4cdef5fcef145
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-29 00:14:15 +0300

    chore: Apply Prettier formatting and localization

--- src/components/Filters/SubCategories.tsx
+++ src/components/Filters/SubCategories.tsx
@@ -1,20 +1,22 @@
-import { SUBCATEGORIES } from "@/constants";
+import { useTranslations } from 'next-intl'
+import { SUBCATEGORIES } from '@/constants'
 
 const SubCategories = () => {
+  const t = useTranslations('Home')
   return (
-    <ul className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium dark:text-stone-100 text-gray-900">
+    <ul className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900 dark:text-stone-100">
       {SUBCATEGORIES.map((category) => (
         <li key={category.name}>
           <button
             disabled={!category.selected}
             className="disabled:cursor-not-allowed disabled:opacity-60"
           >
-            {category.name}
+            {t(category.name)}
           </button>
         </li>
       ))}
     </ul>
-  );
-};
+  )
+}
 
-export default SubCategories;
+export default SubCategories
