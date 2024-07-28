078450e8bad803675640659df014e815e6b9bc58
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-29 00:11:39 +0300

    style: Apply Prettier formatting

--- src/components/theme/ModeToggle.tsx
+++ src/components/theme/ModeToggle.tsx
@@ -1,52 +1,54 @@
-"use client";
+'use client'
 
-import * as React from "react";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
+import * as React from 'react'
 
-import { Button } from "@/components/ui/button";
+import { useTheme } from 'next-themes'
+import { Moon, Sun } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+
 const ModeToggle = () => {
-  const { setTheme } = useTheme();
+  const { setTheme } = useTheme()
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
         <Button
-          className="dark:bg-slate-500 bg-slate-800 hover:bg-slate-700 "
+          className="bg-sky-900 hover:bg-sky-800 dark:bg-slate-700"
           size="icon"
         >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 text-slate-100 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="dark:bg-slate-900  " align="end">
+      <DropdownMenuContent className="dark:bg-slate-900" align="end">
         <DropdownMenuItem
-          className="dark:bg-slate-900 dark:focus:bg-slate-500  dark:hover:bg-slate-500 "
-          onClick={() => setTheme("light")}
+          className="dark:bg-slate-900 dark:hover:bg-slate-500 dark:focus:bg-slate-500"
+          onClick={() => setTheme('light')}
         >
           Light
         </DropdownMenuItem>
         <DropdownMenuItem
-          className="dark:bg-slate-900  dark:focus:bg-slate-500 dark:hover:bg-slate-500"
-          onClick={() => setTheme("dark")}
+          className="dark:bg-slate-900 dark:hover:bg-slate-500 dark:focus:bg-slate-500"
+          onClick={() => setTheme('dark')}
         >
           Dark
         </DropdownMenuItem>
         <DropdownMenuItem
-          className="dark:bg-slate-900  dark:focus:bg-slate-500 dark:hover:bg-slate-500"
-          onClick={() => setTheme("system")}
+          className="dark:bg-slate-900 dark:hover:bg-slate-500 dark:focus:bg-slate-500"
+          onClick={() => setTheme('system')}
         >
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  );
-};
+  )
+}
 
-export default ModeToggle;
+export default ModeToggle
