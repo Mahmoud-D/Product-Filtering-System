c568c148fbb9bb23aedde1894d08b13ec6a65aeb
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-29 00:16:10 +0300

    chore: Apply Prettier formatting and localization

--- src/components/Filters/PriceFilter.tsx
+++ src/components/Filters/PriceFilter.tsx
@@ -1,61 +1,62 @@
-import { SetStateAction } from "react";
-import { QueryObserverResult } from "@tanstack/react-query";
-import { QueryResult } from "@upstash/vector";
-import { DebouncedFunc } from "lodash";
+import { SetStateAction } from 'react'
+import { QueryObserverResult } from '@tanstack/react-query'
+import { QueryResult } from '@upstash/vector'
+import { useTranslations } from 'next-intl'
+import { DebouncedFunc } from 'lodash'
 
 import {
   AccordionContent,
   AccordionItem,
-  AccordionTrigger,
-} from "../ui/accordion";
-import { Slider } from "../ui/slider";
+  AccordionTrigger
+} from '../ui/accordion'
+import { Slider } from '../ui/slider'
 
-import { cn } from "@/lib/utils";
-import { TProductState } from "@/lib/validators/product-validator";
-
-import { TProduct } from "@/types";
-import { DEFAULT_CUSTOM_RANGE, PRICE_FILTERS } from "@/constants";
+import { DEFAULT_CUSTOM_RANGE, PRICE_FILTERS } from '@/constants'
+import { cn } from '@/lib/utils'
+import { TProductState } from '@/lib/validators/product-validator'
+import { TProduct } from '@/types'
 
 type TProps = {
-  filter: TProductState;
-  setFilter: (value: SetStateAction<TProductState>) => void;
+  filter: TProductState
+  setFilter: (value: SetStateAction<TProductState>) => void
   stableDebouncedRefetch:
     | DebouncedFunc<
         () => Promise<QueryObserverResult<QueryResult<TProduct>[], Error>>
       >
-    | undefined;
-};
+    | undefined
+}
 const PriceFilter = ({ filter, setFilter, stableDebouncedRefetch }: TProps) => {
-  const minPrice = Math.min(filter.price.range[0], filter.price.range[1]);
-  const maxPrice = Math.max(filter.price.range[0], filter.price.range[1]);
+  const t = useTranslations('Home')
+  const minPrice = Math.min(filter.price.range[0], filter.price.range[1])
+  const maxPrice = Math.max(filter.price.range[0], filter.price.range[1])
   return (
     <AccordionItem value="price">
-      <AccordionTrigger className="py-3 text-sm dark:text-stone-100 dark:hover:text-stone-200  text-gray-400 hover:text-gray-500 ">
-        <span className="font-medium dark:text-stone-100 text-gray-900">
-          Price
+      <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500 dark:text-stone-100 dark:hover:text-stone-200">
+        <span className="font-medium text-gray-900 dark:text-stone-100">
+          {t('Price')}
         </span>
       </AccordionTrigger>
-      <AccordionContent className=" pt-6 animate-none">
+      <AccordionContent className="animate-none pt-6">
         <ul className="space-y-4">
           {PRICE_FILTERS.options.map((option, optionIdx) => (
             <li
               key={option.label}
-              className="flex items-center group cursor-pointer"
+              className="group flex cursor-pointer items-center"
             >
               <input
                 type="radio"
                 id={`price-${optionIdx}`}
-                className="h-4 w-4 rounded border-gray-300 text-indigo-600 group-hover:cursor-pointer focus:ring-indigo-500"
+                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 group-hover:cursor-pointer"
                 onChange={() => {
                   setFilter((prev) => ({
                     ...prev,
                     price: {
                       isCustom: false,
-                      range: [...option.value],
-                    },
-                  }));
+                      range: [...option.value]
+                    }
+                  }))
                   if (stableDebouncedRefetch) {
-                    stableDebouncedRefetch();
+                    stableDebouncedRefetch()
                   }
                 }}
                 checked={
@@ -66,13 +67,13 @@ const PriceFilter = ({ filter, setFilter, stableDebouncedRefetch }: TProps) => {
               />
               <label
                 htmlFor={`price-${optionIdx}`}
-                className="mx-3 text-sm dark:text-stone-100 text-gray-600 group-hover:cursor-pointer"
+                className="mx-3 text-sm text-gray-600 group-hover:cursor-pointer dark:text-stone-100"
               >
-                {option.label}
+                {t(option.label)}
               </label>
             </li>
           ))}
-          <li className="flex justify-center flex-col group cursor-pointer">
+          <li className="group flex cursor-pointer flex-col justify-center">
             <div>
               <input
                 type="radio"
@@ -82,11 +83,11 @@ const PriceFilter = ({ filter, setFilter, stableDebouncedRefetch }: TProps) => {
                     ...prev,
                     price: {
                       isCustom: true,
-                      range: [0, 100],
-                    },
-                  }));
+                      range: [0, 100]
+                    }
+                  }))
                   if (stableDebouncedRefetch) {
-                    stableDebouncedRefetch();
+                    stableDebouncedRefetch()
                   }
                 }}
                 checked={filter.price.isCustom}
@@ -94,14 +95,14 @@ const PriceFilter = ({ filter, setFilter, stableDebouncedRefetch }: TProps) => {
               />
               <label
                 htmlFor={`price-${PRICE_FILTERS.options.length}`}
-                className="mx-3 text-sm cursor-pointer dark:text-stone-100 text-gray-600"
+                className="mx-3 cursor-pointer text-sm text-gray-600 dark:text-stone-100"
               >
-                Custom
+                {t('Custom')}
               </label>
             </div>
 
-            <div className="flex justify-between mt-2 mb-2 items-center ">
-              <p className="font-medium">Price</p>
+            <div className="mb-2 mt-2 flex items-center justify-between">
+              <p className="font-medium">{t('Price')}</p>
               <div>
                 {filter.price.isCustom
                   ? minPrice.toFixed(0)
@@ -115,7 +116,7 @@ const PriceFilter = ({ filter, setFilter, stableDebouncedRefetch }: TProps) => {
             </div>
             <Slider
               className={cn({
-                "opacity-50": !filter.price.isCustom,
+                'opacity-50': !filter.price.isCustom
               })}
               min={DEFAULT_CUSTOM_RANGE[0]}
               max={DEFAULT_CUSTOM_RANGE[1]}
@@ -128,16 +129,16 @@ const PriceFilter = ({ filter, setFilter, stableDebouncedRefetch }: TProps) => {
               }
               disabled={!filter.price.isCustom}
               onValueChange={(range) => {
-                const [minValue, maxValue] = range;
+                const [minValue, maxValue] = range
                 setFilter((prev) => ({
                   ...prev,
                   price: {
                     ...prev.price,
-                    range: [minValue, maxValue],
-                  },
-                }));
+                    range: [minValue, maxValue]
+                  }
+                }))
                 if (stableDebouncedRefetch) {
-                  stableDebouncedRefetch();
+                  stableDebouncedRefetch()
                 }
               }}
             />
@@ -145,7 +146,7 @@ const PriceFilter = ({ filter, setFilter, stableDebouncedRefetch }: TProps) => {
         </ul>
       </AccordionContent>
     </AccordionItem>
-  );
-};
+  )
+}
 
-export default PriceFilter;
+export default PriceFilter
