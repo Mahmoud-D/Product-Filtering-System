d6233eb10436173741b9eff6c3f6d74a26086538
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-29 00:13:33 +0300

    chore: Apply Prettier formatting and localization

--- src/components/products/ProductItem.tsx
+++ src/components/products/ProductItem.tsx
@@ -1,37 +1,42 @@
-import Image from "next/image";
-import { TProduct } from "@/types";
+import Image from 'next/image'
+import { useTranslations } from 'next-intl'
+
+import { TProduct } from '@/types'
 
 type TProps = {
-  product: TProduct;
-};
+  product: TProduct
+}
 
 const ProductItem = ({ product }: TProps) => {
+  const t = useTranslations('Home')
   return (
     <li className="group relative">
-      <figure className="aspect-h-1 aspect-w-1 w-full overflow-hidden dark:bg-stone-300 rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+      <figure className="aspect-h-1 aspect-w-1 lg:aspect-none w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 dark:bg-stone-300 lg:h-80">
         <Image
           src={product.imageId}
           alt="product Image"
           width={500}
           height={500}
-          className="w-full h-full object-cover object-center"
+          className="h-full w-full object-cover object-center"
         />
       </figure>
 
       <div className="mt-4 flex justify-between">
         <div>
-          <h3 className="text-sm dark:text-stone-300 text-gray-700">
-            {product.name}
+          <h3 className="text-sm text-gray-700 dark:text-stone-300">
+            {t(product.name)}
           </h3>
-          <p className="mt-1 text-sm dark:text-stone-200 text-gray-500">
-            Size {product.size.toUpperCase()}, {product.color}
+          <p className="mt-1 text-sm text-gray-500 dark:text-stone-200">
+            {t('Size')} {t(product.size.toUpperCase())}, {t(product.color)}
           </p>
         </div>
 
-        <p className="text-sm font-medium text-gray-900">{product.price}</p>
+        <p className="text-sm font-medium text-gray-900 dark:text-stone-200">
+          {product.price}
+        </p>
       </div>
     </li>
-  );
-};
+  )
+}
 
-export default ProductItem;
+export default ProductItem
