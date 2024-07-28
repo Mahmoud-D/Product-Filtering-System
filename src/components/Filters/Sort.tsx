1dc52965b90a0750745a4501c0706f03d278dccd
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-29 00:15:20 +0300

    chore: Apply Prettier formatting and localization

--- src/components/Filters/Sort.tsx
+++ src/components/Filters/Sort.tsx
@@ -1,65 +1,66 @@
-import { SetStateAction } from "react";
-import { QueryObserverResult } from "@tanstack/react-query";
-import { QueryResult } from "@upstash/vector";
-import { DebouncedFunc } from "lodash";
+import { SetStateAction } from 'react'
+import { QueryObserverResult } from '@tanstack/react-query'
+import { QueryResult } from '@upstash/vector'
+import { useTranslations } from 'next-intl'
+import { DebouncedFunc } from 'lodash'
+import { ChevronDown } from 'lucide-react'
 
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "../ui/dropdown-menu";
+  DropdownMenuTrigger
+} from '../ui/dropdown-menu'
 
-import { TProductState } from "@/lib/validators/product-validator";
-import { cn } from "@/lib/utils";
-
-import { SORT_OPTIONS } from "@/constants";
-import { TProduct } from "@/types";
-
-import { ChevronDown } from "lucide-react";
+import { SORT_OPTIONS } from '@/constants'
+import { cn } from '@/lib/utils'
+import { TProductState } from '@/lib/validators/product-validator'
+import { TProduct } from '@/types'
 
 type TProps = {
-  filter: TProductState;
-  setFilter: (value: SetStateAction<TProductState>) => void;
+  filter: TProductState
+  setFilter: (value: SetStateAction<TProductState>) => void
   stableDebouncedRefetch:
     | DebouncedFunc<
         () => Promise<QueryObserverResult<QueryResult<TProduct>[], Error>>
       >
-    | undefined;
-};
+    | undefined
+}
 const Sort = ({ filter, setFilter, stableDebouncedRefetch }: TProps) => {
+  const t = useTranslations('Home')
   return (
     <div className="flex items-center">
-      <DropdownMenu>
-        <DropdownMenuTrigger className="group inline-flex justify-center text-lg items-center font-medium dark:text-stone-100 dark:hover:text-stone-300 text-gray-700 hover:text-gray-900 ">
-          sort
-          <ChevronDown className="mr-1 ml-1 h-5 w-5 flex-shrink-0  dark:text-stone-100 dark:hover:text-stone-300 text-gray-400 group-hover:text-gray-500 " />
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger className="group inline-flex items-center justify-center text-lg font-medium text-gray-700 hover:text-gray-900 dark:text-stone-100 dark:hover:text-stone-300">
+          {t('sort')}
+
+          <ChevronDown className="ml-1 mr-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500 dark:text-stone-100 dark:hover:text-stone-300" />
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           {SORT_OPTIONS.map((option) => (
             <button
               key={option.name}
               className={cn(
-                "block w-full dark:bg-slate-900 text-sm px-4 py-2",
+                'block w-full px-4 py-2 text-sm dark:bg-slate-900',
                 {
-                  "font-bold text-gray-900 bg-gray-100 dark:text-stone-100 dark:bg-slate-500 ":
+                  'bg-gray-100 font-bold text-gray-900 dark:bg-slate-500 dark:text-stone-100':
                     filter.sort === option.value,
-                  "text-gray-300": filter.sort !== option.value,
+                  'text-gray-300': filter.sort !== option.value
                 }
               )}
               onClick={() => {
-                setFilter((prev) => ({ ...prev, sort: option.value }));
+                setFilter((prev) => ({ ...prev, sort: option.value }))
                 if (stableDebouncedRefetch) {
-                  stableDebouncedRefetch();
+                  stableDebouncedRefetch()
                 }
               }}
             >
-              {option.name}
+              {t(option.name)}
             </button>
           ))}
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
-  );
-};
+  )
+}
 
-export default Sort;
+export default Sort
