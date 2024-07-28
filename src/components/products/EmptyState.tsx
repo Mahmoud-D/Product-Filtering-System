24639048d48684be65da177d55b6c3af3f6d35c1
Mahmoud Ahmed <Mahmoud.A.Dash@gmail.com>
2024-07-29 00:13:55 +0300

    chore: Apply Prettier formatting and localization

--- src/components/products/EmptyState.tsx
+++ src/components/products/EmptyState.tsx
@@ -1,18 +1,21 @@
-import { XCircle } from "lucide-react";
+import { useTranslations } from 'next-intl'
+import { XCircle } from 'lucide-react'
 
 const EmptyState = () => {
+  const t = useTranslations('Home')
   return (
-    <div className="relative col-span-full h-80 bg-gray-50 w-full p-12 flex flex-col items-center justify-center">
+    <div className="relative col-span-full flex h-80 w-full flex-col items-center justify-center bg-gray-50 p-12">
       <XCircle className="h-10 w-10 text-red-500" />
-      <h3 className="font-bold my-2 text-stone-900  text-3xl capitalize ">
-        No products found
+      <h3 className="my-2 text-3xl font-bold capitalize text-stone-900">
+        {t('No products found')}
       </h3>
-      <p className="text-zinc-500 text-sm  capitalize ">
-        No search results match your query. Please adjust your search and try
-        again.
+      <p className="text-sm capitalize text-zinc-500">
+        {t(
+          'No search results match your query. Please adjust your search and try again.'
+        )}
       </p>
     </div>
-  );
-};
+  )
+}
 
-export default EmptyState;
+export default EmptyState
